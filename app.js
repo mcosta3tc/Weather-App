dcc56b6629dad343be9fa08d1156304e6e67fc9e
m_costa3 <m_costa3@etu-webschoolfactory.fr>
2020-07-01 16:42:24 +0200

    card design & title updated

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
-const searchElement = document.querySelector('#city-search');
 
 //APP Data
 //Objet weather qui se remplira avec notre api
